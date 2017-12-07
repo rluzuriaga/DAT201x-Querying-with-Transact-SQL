ae620c4ee7be5ddada0fcd5d634f126af87d867d
rluzuriaga <rluzuriagaaguirre@student.mtsac.edu>
2017-12-06 19:25:46 -0800

    Added questions to queries

--- Module 2/Lab 2 Exercises.sql
+++ Module 2/Lab 2 Exercises.sql
@@ -1,35 +1,47 @@
 -- Q1
+-- select unique cities, and state province
 SELECT DISTINCT City, StateProvince
 FROM SalesLT.Address;
 
 -- Q2
+-- select the top 10 percent from the Name column
 SELECT TOP 10 PERCENT Name
 FROM SalesLT.Product
+-- order by the weight in descending order
 ORDER BY Weight DESC;
 
 -- Q3
 SELECT Name
 FROM SalesLT.Product
 ORDER BY Weight DESC
+-- offset 10 rows and get the next 100
 OFFSET 10 ROWS FETCH NEXT 100 ROWS ONLY;
 
 -- Q4
+-- select the Name, Color, and Size columns
 SELECT Name, Color, Size
 FROM SalesLT.Product
+-- check ProductModelID is 1
 WHERE ProductModelID = 1;
 
 -- Q5
+-- select the ProductNumber and Name columns
 SELECT ProductNumber, Name
 FROM SalesLT.Product
+-- check that Color is one of 'Black', 'Red' or 'White'
+-- check that Size is one of 'S' or 'M'
 WHERE Color IN ('Black', 'Red', 'White') AND Size IN ('S', 'M');
 
 -- Q6
+-- select the ProductNumber, Name, and ListPrice columns
 SELECT ProductNumber, Name, ListPrice
 FROM SalesLT.Product
+-- filter for product numbers beginning with BK- using LIKE
 WHERE ProductNumber LIKE 'BK%';
 
-SELECT ProductNumber FROM SalesLT.Product;
 --Q7
+-- select the ProductNumber, Name, and ListPrice columns
 SELECT ProductNumber, Name, ListPrice
 FROM SalesLT.Product
+-- filter for ProductNumbers
 WHERE ProductNumber LIKE 'BK-[^R]%-[0-9][0-9]';
